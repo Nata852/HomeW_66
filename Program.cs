49c1144297427b4cd664adb94144505dc06ade55
Natali <natlyly3@mail.ru>
2023-02-27 20:13:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int GetNumber(string message)
+{
+    int resalt = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out resalt))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+    return resalt;
+}
+
+int SumRec(int m, int n)
+{
+    
+    if (m == n) return 0;
+
+    else
+    {
+        m++;
+        return m + SumRec(m, n);
+    }
+}
+int m = GetNumber("Введите M -> ");
+int n = GetNumber("Введите N -> ");
+Console.WriteLine(SumRec(m-1, n));
